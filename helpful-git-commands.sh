efcd2c4b5e9f9dd961c7c57df2d6090b12435012
kelshmo <kelsey.montgomery@sagebase.org>
2021-06-04 13:58:18 -0500

    track notes in txt not bash

--- helpful-git-commands.sh
+++
@@ -1,4 +0,0 @@
-# This script will document useful git commands. 
-git clone # this creates a local copy of a github respository
-git remote # this command visualizes the remote I'm connected to
-# by default our remote is called origin
