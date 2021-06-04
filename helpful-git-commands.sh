09fc58a3237cc77c31039ee00fd272b122426659
kelshmo <kelsey.montgomery@sagebase.org>
2021-06-04 11:37:57 -0500

    mario and kels will collab on this doc

---
+++ helpful-git-commands.sh
@@ -0,0 +1,4 @@
+# This script will document useful git commands. 
+git clone # this creates a local copy of a github respository
+git remote # this command visualizes the remote I'm connected to
+# by default our remote is called origin
