0cec2a7402bc466ae19c8978fac310f63f1d8d44
Mario Hevia <mario.hevia@sabase.org>
2021-06-24 10:16:19 -0700

    Add interactivity with user for selection of syn-objects, in this case parent folder, how many data sets you planning to joint, and its respective syn-numbers

--- syn_pandas.py
+++ syn_pandas.py
@@ -9,13 +9,23 @@ syn = sc.Synapse()
 syn = sc.login()
 
 ## list of the files that we will use
-entity_list =['syn25878115', 'syn25878114', 'syn25878112']
+## entity_list =['syn25878115', 'syn25878114', 'syn25878112']
+## parentId = syn25878110
+## adding interactivity with user  
+entity_list = []
+parent_id = str(input('Plese enter the parent ID folder in the syn12345678 format: '))
+len_entity_list = int(input('how many data frames are you planning to joint?'))
+
+for i in range(1, len_entity_list+1):
+       entity_list.append(str(input('please enter syn num {}:'.format(i))))
+print('\nlist of syns to joint: {},\nParent folder: {}'.format(entity_list, parent_id))
 
 ## Code to download selected in current location
 ## Defining the frame and assigning the path where the entity is
 ## lowering and striping of white spaces at the beggining and end of the column name
 ## renaming the columns where we will joint
-## merging on fips and inner.
+## merging an inner joint on fips
+
 li= [] # empty list to recive the list of frames to reduce
 for items in entity_list:
         entity = syn.get(items,downloadFile=True, 
@@ -27,13 +37,8 @@ for items in entity_list:
         df.rename(columns = {'countyid':'fips'}, inplace = True)
         df_merged = reduce(lambda left, right:pd.merge(left, right, on=['fips'], how='inner'),li)
         # reduce a function to an iterable and reduce it to a single cumulative value.
-df_merged.to_csv('big_df.csv')# changing into csv
-version_1 = syn.store(File('big_df.csv', parentId='syn25878110'), used=entity_list)
-# project = syn.store(version_1, used=entity_list, used)
+df_merged.to_csv('big_df2.csv')# changing into csv
+version_2 = syn.store(File('big_df2.csv', parentId=parent_id), used=entity_list)
 
-############################
-## to upload to synapse
-## the folowing code was used to upload the merged df
-## please remove hashtag if we need to run upload (uploaded already)
-#version_1 = syn.store(File('big_df.csv', parentId='syn25878110', used=used_files))
-#project = syn.store(version_1)# list of input files and IDS
+#project = syn.store(version_2)
+
